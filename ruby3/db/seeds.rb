271129867dd383ce45cb1c212c6a958d5228124d
ma2k8 <matsukawa@thealp.co.jp>
2021-08-08 23:26:40 +0900

    add seed data

--- ruby3/db/seeds.rb
+++ ruby3/db/seeds.rb
@@ -5,3 +5,5 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+(1..10000).each{ |i| User.create(name: i) }
